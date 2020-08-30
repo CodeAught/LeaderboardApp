568aa1bd2a307dac44192dd784d06c63bac21ffb
CodeAught <codeaught@gmail.com>
2020-08-30 12:11:38 +0530

    Added all functionalities in leaderboard service

--- src/app/services/leaderboard.service.ts
+++ src/app/services/leaderboard.service.ts
@@ -2,28 +2,95 @@ import { Injectable } from '@angular/core';
 
 import { ILeaderboardService } from '../interfaces/leaderboard-service.interface'
 import { Leader } from '../interfaces/leader.interface';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LeaderboardService implements ILeaderboardService {
-  list: Array<Leader>
+  private readonly maxLimit = 1000;
 
-  constructor() { }
+  list: Array<Leader> = [
+    {
+      id: '111',
+      name: 'Leader 1',
+      points: 55
+    },
+    {
+      id: '114',
+      name: 'Leader 4',
+      points: 67
+    },
+    {
+      id: '112',
+      name: 'Leader 2',
+      points: 85
+    },
+    {
+      id: '113',
+      name: 'Leader 3',
+      points: 75
+    }
+  ];
+
+  private leaders$: BehaviorSubject<Array<Leader>> = new BehaviorSubject(this.list);
+
+  constructor() {
+    this.sortLeaders();
+  }
+
+  getLeaders(): Observable<Array<Leader>> {
+    return this.leaders$.asObservable();
+  }
+
+  generateId(): string {
+    return Math.floor(this.maxLimit * Math.random()).toString();
+  }
 
   insert(name: string): void {
+    const leader = {
+      id: this.generateId(),
+      name: name,
+      points: 0
+    };
 
+    this.list.push(leader);
+    this.publishUpdatedList();
   }
 
   getList(): Array<Leader> {
-    return;
+    return [...this.list];
   }
 
   increment(id: string): void {
-
+    const index = this.findLeaderIndex(id);
+    if(index >= 0) {
+      this.list[index].points = ++this.list[index].points;
+      this.sortLeaders();
+    }
   }
 
   decrement(id: string): void {
+    const index = this.findLeaderIndex(id);
+    if(index >= 0) {
+      this.list[index].points = --this.list[index].points;
+      this.sortLeaders();
+    }
+  }
 
+  findLeaderIndex(id: string): number {
+    return this.list?.findIndex(leader => leader.id === id);
+  }
+
+  sortLeaders() {
+    this.list?.sort((leader1, leader2) => {
+      return leader2.points - leader1.points;
+    });
+
+    this.publishUpdatedList();
+  }
+
+  publishUpdatedList() {
+    this.leaders$.next(this.list);
   }
 }

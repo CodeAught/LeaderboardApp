6130641446ce9696da8b7d0513065d8c472e811b
CodeAught <codeaught@gmail.com>
2020-08-30 08:05:23 +0530

    Imported initially required material components

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule
+  ],
+  exports: [
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }

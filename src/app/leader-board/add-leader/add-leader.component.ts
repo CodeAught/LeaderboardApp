0c97ef1540160bf5c5906d614179259e65249931
CodeAught <codeaught@gmail.com>
2020-08-30 08:35:55 +0530

    Created component to add leader

---
+++ src/app/leader-board/add-leader/add-leader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-leader',
+  templateUrl: './add-leader.component.html',
+  styleUrls: ['./add-leader.component.css']
+})
+export class AddLeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/leader-board/leader-board.module.ts
+++ src/app/leader-board/leader-board.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { MaterialModule } from '../material/material.module';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { AddLeaderComponent } from './add-leader/add-leader.component';
 
 
 @NgModule({
-  declarations: [LeaderboardComponent],
+  declarations: [LeaderboardComponent, AddLeaderComponent],
   imports: [
     MaterialModule
   ]

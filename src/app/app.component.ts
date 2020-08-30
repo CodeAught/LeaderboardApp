0190d0756d66a7f4bdd1914953852b839fdcfd4c
CodeAught <codeaught@gmail.com>
2020-08-30 12:13:39 +0530

    Injected leaderboard service & using its services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { LeaderboardService } from './services/leaderboard.service';
+import { Leader } from './interfaces/leader.interface';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'LeaderboardApp';
+export class AppComponent implements OnInit {
+  title = 'Leaderboard App';
+  leaders$: Observable<Array<Leader>>;
+
+  constructor(private leaderboardService: LeaderboardService) {}
+
+  ngOnInit(): void {
+    this.getLeaders();
+  }
+
+  onIncrement(id: string): void {
+    this.leaderboardService.increment(id);
+  }
+
+  onDecrement(id: string): void {
+    this.leaderboardService.decrement(id);
+  }
+
+  getLeaders(): void {
+    this.leaders$ = this.leaderboardService.getLeaders();
+  }
+
 }

f7b14e3cebe2b2ea935006627acb26d041776e72
CodeAught <codeaught@gmail.com>
2020-08-30 13:55:58 +0530

    Added functionality to add new leader

--- src/app/leader-board/add-leader/add-leader.component.ts
+++ src/app/leader-board/add-leader/add-leader.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, Output, EventEmitter } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-add-leader',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-leader.component.css']
 })
 export class AddLeaderComponent implements OnInit {
+  @Output() addLeader: EventEmitter<string> = new EventEmitter();
+  name: string;
 
-  constructor() { }
+  private dialogRef: MatDialogRef<any>;
+
+  horizontalPosition: MatSnackBarHorizontalPosition = 'start';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+
+  constructor(private dialog: MatDialog, private snackbar: MatSnackBar) { }
 
   ngOnInit(): void {
+  }
+
+  onAddLeader(modal: TemplateRef<any>) {
+    this.dialogRef = this.dialog.open(modal);
+    this.dialogRef.afterClosed().subscribe(name => {
+      this.addLeader.emit(name);
+      this.openSnackBar();
+      this.name = '';
+    });
+  }
+
+  openSnackBar() {
+    const config = {
+      duration: 3000,
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition
+    };
+    this.snackbar.open('Added to leaderboard', 'X', config);
+  }
+
+  close() {
+    this.dialogRef.close(this.name);
   }
 
 }

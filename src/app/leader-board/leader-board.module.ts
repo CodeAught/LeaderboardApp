9f510059dcebc775211228fca51b00c856880946
CodeAught <codeaught@gmail.com>
2020-08-30 12:08:59 +0530

    Added leader component in declarations array

--- src/app/leader-board/leader-board.module.ts
+++ src/app/leader-board/leader-board.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from '../material/material.module';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 import { AddLeaderComponent } from './add-leader/add-leader.component';
+import { LeaderComponent } from './leader/leader.component';
 
 
 @NgModule({
-  declarations: [LeaderboardComponent, AddLeaderComponent],
+  declarations: [LeaderboardComponent, AddLeaderComponent, LeaderComponent],
   imports: [
     MaterialModule
   ],

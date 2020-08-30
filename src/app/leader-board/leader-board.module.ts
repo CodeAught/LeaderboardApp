faee582954fcde11b97ec7e2c12e9d7194f409f0
CodeAught <codeaught@gmail.com>
2020-08-30 13:54:56 +0530

    Imported FormsModule to use ngModel

--- src/app/leader-board/leader-board.module.ts
+++ src/app/leader-board/leader-board.module.ts
@@ -4,11 +4,13 @@ import { MaterialModule } from '../material/material.module';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 import { AddLeaderComponent } from './add-leader/add-leader.component';
 import { LeaderComponent } from './leader/leader.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [LeaderboardComponent, AddLeaderComponent, LeaderComponent],
   imports: [
+    FormsModule,
     MaterialModule
   ],
   exports: [

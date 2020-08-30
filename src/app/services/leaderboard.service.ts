ad0e1dda61bf3dc210deab074722303bda826742
CodeAught <codeaught@gmail.com>
2020-08-30 09:12:32 +0530

    Implemented leaderboard service interface

--- src/app/services/leaderboard.service.ts
+++ src/app/services/leaderboard.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
 
+import { ILeaderboardService } from '../interfaces/leaderboard-service.interface'
+import { Leader } from '../interfaces/leader.interface';
+
 @Injectable({
   providedIn: 'root'
 })
-export class LeaderboardService {
+export class LeaderboardService implements ILeaderboardService {
+  list: Array<Leader>
 
   constructor() { }
+
+  insert(name: string): void {
+
+  }
+
+  getList(): Array<Leader> {
+    return;
+  }
+
+  increment(id: string): void {
+
+  }
+
+  decrement(id: string): void {
+
+  }
 }
